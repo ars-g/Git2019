31938343c755502cfeeb85fa9124f396eaccce73
ars-g <49622106+ars-g@users.noreply.github.com>
2019-04-15 10:39:21 +0430

    Day27

--- app/src/main/java/com/example/git2019/MainActivity.java
+++ app/src/main/java/com/example/git2019/MainActivity.java
@@ -9,5 +9,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //THis Is A Test
+        //adkjfwugnwbugwwefjognb
+        //nndjfjeiqfnnvds;a
+        //its wasnt in update version
+        //qefiewfnewlfk
+
+        //adfafgqqefqd
+        //sadafaqef
     }
 }
